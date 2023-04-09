5c5a42a36ef1b3dfd1dec3013cfde272d2018dcd
RiKi <phoenixarmand.ani@georgebrown.ca>
2023-04-09 02:48:24 -0400

    COMP3133-Assignment2 Progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog'
+import { EmployeeSettingsComponent } from './employee-settings/employee-settings.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'COMP3133-Assignment2';
+
+  constructor(private _dialog: MatDialog) {}
+
+  openAddEditEmpForm(){
+    this._dialog.open(EmployeeSettingsComponent)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatButtonModule} from '@angular/material/button';
+import { EmployeeSettingsComponent } from './employee-settings/employee-settings.component'; 
+import { MatDialogModule } from '@angular/material/dialog'
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import {MatInput, MatInputModule} from '@angular/material/input'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeSettingsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
